c84d970aed77117de926c5f6313501050103f518
Mia Kraft <mia@brightfuturetech.se>
2023-02-09 16:41:36 +0100

    Init w CRA - tailwind, fonts, images+test in place

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import { SBABMockup } from './components/SBABMockup';
+import './fonts/SBAB-Regular.woff';
+
+export default function App() {
+  return (
+    <SBABMockup />
+  )
+}
---
+++ src/components/SBABMockup.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Compare from "./../images/svg/Compare.svg";
+import Percentage from "./../images/svg/Percentage.svg";
+    
+
+export const SBABMockup = () => {
+  return (
+    <>
+    <img src={Compare} alt="Compare" />
+    <img src={Percentage} alt="Percentage" />
+    <h1 className="text-4xl font-bold underline">
+      Hello SBAB!
+    </h1>
+    </>
+  )
+}
